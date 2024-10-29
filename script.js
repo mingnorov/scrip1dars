f23b8b2a6353f0fdca25daf0832ed4943324db03
mingnorov <zovlonmingnorov@gmail.com>
2024-10-29 19:58:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let age = ('Oyin boshlandi');
+alert(age);
+
+let user = prompt('Oyinga tayor misan');
+alert(user);
+
+let one = prompt('sani ismin nima?');
+alert(one);
+
+let  two = confirm('davom etishga tayormisan?');
+alert(two)
